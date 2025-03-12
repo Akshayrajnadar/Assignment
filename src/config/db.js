23eaaf90c88d2fd012e1b0069156a90a87c7c735
Akshayraj Nadar <akshayraj08032000@gmail.com>
2025-03-12 15:22:19 +0530

    changes in db

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,16 @@
-require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.DB_PORT || 3306,
+    connectionLimit: 10,
 });
+
+pool.getConnection()
+    .then(() => console.log('✅ Connected to MySQL on Railway'))
+    .catch(err => console.error('❌ MySQL Connection Error:', err));
 
 module.exports = pool;
